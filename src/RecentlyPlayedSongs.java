a93c3bb51c199206cdcceaa6713d625710e4e2eb
kattasaratchandra <kattasaratchandra@gmail.com>
2023-03-22 02:44:19 +0530

    tested with examples

--- src/RecentlyPlayedSongs.java
+++ src/RecentlyPlayedSongs.java
@@ -22,4 +22,17 @@ public class RecentlyPlayedSongs {
         return new ArrayList<>(songs);
     }
 
+    public static void main(String[] args) {
+        RecentlyPlayedSongs store = new RecentlyPlayedSongs(3);
+        store.addSong("user1", "S1");
+        store.addSong("user1", "S2");
+        store.addSong("user1", "S3");
+        System.out.println(store.getRecentlyPlayed("user1")); // [S1, S2, S3]
+        store.addSong("user1", "S4");
+        System.out.println(store.getRecentlyPlayed("user1")); // [S2, S3, S4]
+        store.addSong("user1", "S2");
+        System.out.println(store.getRecentlyPlayed("user1")); // [S3, S4, S2]
+        store.addSong("user1", "S1");
+        System.out.println(store.getRecentlyPlayed("user1")); // [S4, S2, S1]
+    }
 }
