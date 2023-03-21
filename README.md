1e0018ca3af78d49befd0af71b380fd4d964a8dd
kattasaratchandra <kattasaratchandra@gmail.com>
2023-03-22 01:59:02 +0530

    added methods

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Initial thoughts on problem statement, making few points will change accordingly:
 
-1. I will be creating a class name recently played songs, and it will have constructor which initialize the class variables.
-2. In this class I will be creating methods
-   a. To add songs to particular user
-   b. To get recently played songs of particular user which returns the list of songs of that user.
+1. I will be creating a class name recently played songs, and it will have constructor which initialize the 
+class variables.
+2. In this class I will be creating methods 
+   1. To add songs to particular user 
+   2. To get recently played songs of particular user, which returns the list of songs of that user.
 3. I will be using hashMap, linkedList, ArrayList
 4. To eliminate the recently played songs the best data structure to use I think would be linked list. I will be using few methods removeFirst() and addLast() as per problem statement
--- src/RecentlyPlayedSongs.java
+++ src/RecentlyPlayedSongs.java
@@ -1,4 +1,25 @@
+import java.util.*;
 public class RecentlyPlayedSongs {
-
+    // created class variables for fixed capacity and user-song pairs
+    private final int fixedCapacity;
+    private final Map<String, LinkedList<String>> recentlyPlayed;
+    public RecentlyPlayedSongs(int fixedCapacity) {
+        this.fixedCapacity = fixedCapacity;
+        this.recentlyPlayed = new HashMap<>();
+    }
+    // This method adds song to the specific user
+    public void addSong(String user, String song) {
+        LinkedList<String> songs = recentlyPlayed.getOrDefault(user, new LinkedList<>());
+        if (songs.size() == fixedCapacity) {
+            songs.removeFirst();
+        }
+        songs.addLast(song);
+        recentlyPlayed.put(user, songs);
+    }
+    // This method returns the list of songs of the specific user
+    public List<String> getRecentlyPlayed(String user) {
+        LinkedList<String> songs = recentlyPlayed.getOrDefault(user, new LinkedList<>());
+        return new ArrayList<>(songs);
+    }
 
 }
